f1cd41bd63f18c0801e37bf2b04d4f5740fcc003
Ninjas Code - 4 <ninjascode-4@Ninjass-MacBook-Air.local>
2023-04-13 12:40:14 +0500

    server url

--- shopping-cart-admin/src/config/config.js
+++ shopping-cart-admin/src/config/config.js
@@ -1,8 +1,6 @@
 /* eslint-disable camelcase */
-export const server_url = 'http://10.97.28.83:8002/'
-export const ws_server_url = 'ws://10.97.28.83:8002/'
-// export const server_url = 'https://ecommero.ninjascode.com/'
-// export const ws_server_url = 'wss://ecommero.ninjascode.com/'
+export const server_url = 'https://ecommero.ninjascode.com/'
+export const ws_server_url = 'wss://ecommero.ninjascode.com/'
 export const cloudinary_upload_url =
   'https://api.cloudinary.com/v1_1/ecommero/image/upload'
 export const cloudinary_sub_categories = 'd11htdsp'
